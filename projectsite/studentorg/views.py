f97797ce7cb48ac0af02acff3ac04a85a1cc8a03
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-28 20:39:14 +0800

    <option value=prog_name {% if request.GET.sort_by == prog_name %}selected{% endif %}>Program Name</option>

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -34,6 +34,14 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
+    
+    def get_ordering(self):
+        allowed = ["prog_name", "college__college_name"]
+        sort_by = self.request.GET.get("sort_by")
+        if sort_by in allowed:
+            return sort_by
+        return "prog_name"
     
     def get_queryset(self):
         qs = super().get_queryset()
