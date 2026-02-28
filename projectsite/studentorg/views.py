087a99641f5ed390ad077db209127120165e00e9
JM MAYORES <josemiguelmayores@gmail.com>
2026-02-28 20:05:05 +0800

    A. Search Feature and B. Dashboard

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,20 +1,50 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization
+from studentorg.models import Organization, Student, OrgMember 
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
+from django.utils import timezone
+from django.db.models import Q
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
+
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            qs = qs.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return qs
     
 class OrganizationCreateView(CreateView):
     model = Organization
