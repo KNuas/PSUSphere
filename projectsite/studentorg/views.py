7123600e85f013941494b3100f3c39a3cf25f617
6shay9 <zetushiomawa@gmail.com>
2026-03-02 15:13:06 +0800

    Allauth

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -28,60 +28,6 @@ class HomePageView(ListView):
 
         context["students_joined_this_year"] = count
         return context
-
-class OrganizationList(ListView):
-    model = Organization
-    context_object_name = 'organization'
-    template_name = 'org_list.html'
-    paginate_by = 5
-    
-    def get_ordering(self):
-        allowed = ["prog_name", "college__college_name"]
-        sort_by = self.request.GET.get("sort_by")
-        if sort_by in allowed:
-            return sort_by
-        return "prog_name"
-    
-    def get_queryset(self):
-        qs = super().get_queryset()
-        query = self.request.GET.get('q')
-
-        if query:
-            qs = qs.filter(
-                Q(name__icontains=query) |
-                Q(description__icontains=query)
-            )
-        return qs
-    
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["organization_count"] = Organization.objects.count()
-        context["program_count"] = Program.objects.count()
-        return context
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["total_students"] = Student.objects.count()
-
-        today = timezone.now().date()
-        count = (
-            OrgMember.objects.filter(
-                date_joined__year=today.year
-            )
-            .values("student")
-            .distinct()
-            .count()
-        )
-
-        context["students_joined_this_year"] = count
-        return context
-    
-
     
 class OrganizationList(ListView):
     model = Organization
